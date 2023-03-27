dd6ee24ab360292d3e0fb3796b467930522eb816
олег <@DESKTOP-SSTKF2A>
2023-03-27 13:29:48 +0300

    Добавьте файлы проекта.

---
+++ Shadow4/Form1.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace Shadow4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.characterListBox = new System.Windows.Forms.ListBox();
+            this.buyButton = new System.Windows.Forms.Button();
+            this.characterDetailsLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // characterListBox
+            // 
+            this.characterListBox.FormattingEnabled = true;
+            this.characterListBox.Location = new System.Drawing.Point(159, 99);
+            this.characterListBox.Name = "characterListBox";
+            this.characterListBox.Size = new System.Drawing.Size(120, 95);
+            this.characterListBox.TabIndex = 0;
+            this.characterListBox.SelectedIndexChanged += new System.EventHandler(this.characterListBox_SelectedIndexChanged);
+            // 
+            // buyButton
+            // 
+            this.buyButton.Location = new System.Drawing.Point(405, 130);
+            this.buyButton.Name = "buyButton";
+            this.buyButton.Size = new System.Drawing.Size(75, 23);
+            this.buyButton.TabIndex = 1;
+            this.buyButton.Text = "Купити";
+            this.buyButton.UseVisualStyleBackColor = true;
+            this.buyButton.Click += new System.EventHandler(this.buyButton_Click);
+            // 
+            // characterDetailsLabel
+            // 
+            this.characterDetailsLabel.AutoSize = true;
+            this.characterDetailsLabel.Location = new System.Drawing.Point(336, 190);
+            this.characterDetailsLabel.Name = "characterDetailsLabel";
+            this.characterDetailsLabel.Size = new System.Drawing.Size(35, 13);
+            this.characterDetailsLabel.TabIndex = 2;
+            this.characterDetailsLabel.Text = "label1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.characterDetailsLabel);
+            this.Controls.Add(this.buyButton);
+            this.Controls.Add(this.characterListBox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox characterListBox;
+        private System.Windows.Forms.Button buyButton;
+        private System.Windows.Forms.Label characterDetailsLabel;
+    }
+}
+
---
+++ Shadow4/Form1.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Shadow4
+{
+    public partial class Form1 : Form
+    {
+        private List<GameCharacter> characters;
+        public Form1()
+        {
+            InitializeComponent();
+            characters = new List<GameCharacter>
+            {
+                new GameCharacter("Мечник", "Ближній бій", 100),
+                new GameCharacter("Лучник", "Дальній бій", 250),
+                new GameCharacter("Гоблін", "Ближній бій", 300),
+                new GameCharacter("Дракон", "Дальній бій", 600),
+            };
+
+            
+            foreach (GameCharacter character in characters)
+            {
+                characterListBox.Items.Add(character.Name);
+            }
+
+          
+            characterListBox.SelectedIndex = 0;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void characterListBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+           
+            int selectedIndex = characterListBox.SelectedIndex;
+            GameCharacter selectedCharacter = characters[selectedIndex];
+            characterDetailsLabel.Text = $"Name: {selectedCharacter.Name}\nConfiguration: {selectedCharacter.Configuration}\nPrice: ${selectedCharacter.Price}";
+        
+        }
+
+        private void buyButton_Click(object sender, EventArgs e)
+        {
+           
+            int selectedIndex = characterListBox.SelectedIndex;
+            GameCharacter selectedCharacter = characters[selectedIndex];
+
+          
+            bool paymentSuccessful = ProcessPayment(selectedCharacter.Price);
+
+          
+            if (paymentSuccessful)
+            {
+                MessageBox.Show($"Ви успішно здійснили покупку {selectedCharacter.Name} з {selectedCharacter.Configuration} configuration!");
+            }
+            else
+            {
+                MessageBox.Show("Платіж не вдався. Спробуйте ще раз.");
+            }
+        }
+        private bool ProcessPayment(int amount)
+        {
+          
+            return amount <= 100;
+        }
+        public class GameCharacter
+        {
+            public string Name { get; }
+            public string Configuration { get; }
+            public int Price { get; }
+
+            public GameCharacter(string name, string configuration, int price)
+            {
+                Name = name;
+                Configuration = configuration;
+                Price = price;
+            }
+        }
+    }
+}
